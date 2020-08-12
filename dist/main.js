6753ed23ae8939d695f0fec9b0a3f179a0dbc189
wilson-pc <wilverhidalgo@outlook.com>
2020-08-12 16:51:26 -0400

    todo

--- dist/main.js
+++ dist/main.js
@@ -2,9 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const body_parser_1 = require("body-parser");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    await app.listen(3000);
+    app.enableCors();
+    app.use(body_parser_1.json({ limit: '100mb' }));
+    const PORT = process.env.PORT || 3000;
+    await app.listen(PORT);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
